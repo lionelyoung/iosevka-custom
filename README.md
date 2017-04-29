3fd2e9eb80a06385c604d4e1682060bec53e1b74
ly <ly>
2017-04-29 22:03:57 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,38 @@
+Iosevka Custom
+==============
+Installs Mac build requirements for Iosevka and makes my customizations
+
+Homepage: https://github.com/be5invis/Iosevka
+
+Usage
+=====
+
+1. Clone the Iosevka directory and put it in $HOME/Projects
+  ```
+  mkdir $HOME/Projects
+  git clone https://github.com/be5invis/Iosevka
+  ```
+2. Run ./iosevka_custom.sh
+3. Browse to $HOME/Projects/dist/
+  - Your fonts should be in iosevka-custom and will show up as "Iosevka Term" after installation
+
+
+Script will install otfcc and ttfautohint:
+
+- brew tap caryll/homebrew-tap
+- brew install caryll/tap/otfcc-mac64 ttfautohint
+
+Details
+=======
+- low asterisk
+- low underscore
+- single-story g
+- m with shorter middle leg
+- no ligations (term)
+
+The following defaults don't need to be specified:
+
+- fira @ (default)
+- curly braces (default)
+- slashed zero (default)
+- high tilde (default)
---
+++ iosevka_custom.sh
@@ -0,0 +1,47 @@
+#!/usr/bin/env bash
+# Installs requirements and builds my preferences for Iosevka
+#
+# Homepage: https://github.com/be5invis/Iosevka
+#
+# Requirements:
+# - nodejs (≥ 6.0)
+# - ttfautohint
+# - otfcc (≥ 0.7.0)
+
+set -e
+WDIR="$HOME/Projects/Iosevka"
+
+# OS Check
+if [ ! "$(uname)" == "Darwin" ]; then
+  echo "Error: Not on Mac"
+  exit 1
+fi
+
+# Working directory check
+cd $WDIR || exit 1
+
+# Install otfcc and ttfautohint
+brew tap caryll/homebrew-tap
+brew install caryll/tap/otfcc-mac64 ttfautohint
+
+# Create my custom configuration
+# ==============================
+# - low asterisk
+# - low underscore
+# - single-story g
+# - m with shorter middle leg
+# - no ligations (term)
+#
+# The following defaults don't need to be specified:
+# - fira @ (default)
+# - curly braces (default)
+# - slashed zero (default)
+# - high tilde (default)
+make custom-config design=\
+'v-asterisk-low v-underscore-low v-g-singlestorey v-m-shortleg term'
+
+# Build from source
+make custom
+
+# Installation instructions
+echo "Done! Go look in $WDIR/dist/"
